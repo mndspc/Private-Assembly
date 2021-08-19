7c395212cef084269de88dba975409e99f5d4711
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-08-19 09:35:41 +0530

    Push a Class Library Project
    Pushing demo of Class Library

---
+++ GST/GST/GSTCalc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GST
+{
+    //  This class contains functionalities to calculate GST
+    public class GSTCalc
+    {
+        public double ProAmount { get; set; }
+        public double GstPer { get; set; }
+
+        public GSTCalc(double proAmount,double gstPer)
+        {
+            this.ProAmount = proAmount;
+            this.GstPer = gstPer;
+        }
+        public double GstAmount()
+        {
+            double gstAmt = ProAmount / 100 * GstPer;
+            return gstAmt;
+        }
+        public double TotalAmtWithGst()
+        {
+            double gstAmt = GstAmount();
+            double totalWithGst = ProAmount + gstAmt;
+            return totalWithGst;
+        }
+    }
+}
